306d7445eb1610ba0ddcea80650c52ea96034e7e
bpg <bpg1968@gmail.com>
2017-02-23 12:31:49 -0500

    a first attempt at implementing the Alarm class

---
+++ threads/WaitingThread.java
@@ -0,0 +1,30 @@
+package nachos.threads;
+
+import nachos.machine.Lib;
+import nachos.machine.Machine;
+
+public class WaitingThread implements Comparable<WaitingThread> {
+  private static final char DEBUG_FLAG = 'w';
+  private long wakeTime;
+  private KThread thread;
+
+  public WaitingThread(long wakeTime, KThread thread) {
+    Lib.debug(DEBUG_FLAG, Machine.timer().getTime() + " -- WaitingThread.WaitingThread(): START");
+    this.wakeTime = wakeTime;
+    this.thread = thread;
+    Lib.debug(DEBUG_FLAG, Machine.timer().getTime() + " -- WaitingThread.WaitingThread(): END");
+  }
+
+  public long getWakeTime() {
+    return this.wakeTime;
+  }
+
+  public KThread getThread() {
+    return this.thread;
+  }
+
+  @Override
+  public int compareTo(WaitingThread that) {
+    return (int) (this.wakeTime - that.wakeTime);
+  }
+}
