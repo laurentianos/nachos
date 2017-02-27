0156c8415452e7b3a97098699f0fe57c0e5d10ba
aawliyaa <azizawliyaa@gmail.com>
2017-02-27 17:35:23 -0500

    Communicator Class

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -2,84 +2,89 @@ package nachos.threads;
 
 import java.util.LinkedList;
 
+import nachos.machine.*;
+
 /**
  * A <i>communicator</i> allows threads to synchronously exchange 32-bit
- * messages. Multiple threads can be waiting to <i>speak</i>, and multiple
- * threads can be waiting to <i>listen</i>. But there should never be a time
- * when both a speaker and a listener are waiting, because the two threads can
- * be paired off at this point.
+ * messages. Multiple threads can be waiting to <i>speak</i>,
+ * and multiple threads can be waiting to <i>listen</i>. But there should never
+ * be a time when both a speaker and a listener are waiting, because the two
+ * threads can be paired off at this point.
  */
 public class Communicator {
-	/**
-	 * Allocate a new communicator.
-	 */
-	public Communicator() {
+    /**
+     * Allocate a new communicator.
+     */
+    public Communicator() {
+		
 		lock = new Lock();
 		Speaker = new LinkedList<ThreadInfo>();
-		Listener = new LinkedList<ThreadInfo>();
-	}
+		Listener = new LinkedList<ThreadInfo>();	
+    }
 
-	/**
-	 * Wait for a thread to listen through this communicator, and then transfer
-	 * <i>word</i> to the listener.
-	 * 
-	 * <p>
-	 * Does not return until this thread is paired up with a listening thread.
-	 * Exactly one listener should receive <i>word</i>.
-	 * 
-	 * @param word
-	 *            the integer to transfer.
-	 */
-	public void speak(int word) {
-
+    /**
+     * Wait for a thread to listen through this communicator, and then transfer
+     * <i>word</i> to the listener.
+     *
+     * <p>
+     * Does not return until this thread is paired up with a listening thread.
+     * Exactly one listener should receive <i>word</i>.
+     *
+     * @param	word	the integer to transfer.
+     */
+   public void speak(int word) {
+		
 		lock.acquire();
-
-		if (!Listener.isEmpty()) {
-			ThreadInfo listen = Listener.removeFirst();
-			listen.setWord(word);
-			listen.getCondition().wake();
-		}
-		else {
+		
+		if(Listener.isEmpty()){
+			
 			ThreadInfo speaker = new ThreadInfo();
 			speaker.setWord(word);
 			Speaker.add(speaker);
 			speaker.getCondition().sleep();
-		}
+			
+		}else{
+	
+			ThreadInfo listen = Listener.removeFirst();
+			listen.setWord(word);
+			listen.getCondition().wake();
+			
+		}	
 		lock.release();
-	}
+    }
 
-	/**
-	 * Wait for a thread to speak through this communicator, and then return the
-	 * <i>word</i> that thread passed to <tt>speak()</tt>.
-	 * 
-	 * @return the integer transferred.
-	 */
-	public int listen() {
-
+    /**
+     * Wait for a thread to speak through this communicator, and then return
+     * the <i>word</i> that thread passed to <tt>speak()</tt>.
+     *
+     * @return	the integer transferred.
+     */    
+    public int listen() {
+		
 		lock.acquire();
 
 		int word = 0;
-
-		if (!Speaker.isEmpty()) {
-			ThreadInfo speaker = Speaker.removeFirst();
-			word = speaker.getWord();
-			speaker.getCondition().wake();
-		}
-		else {
+		
+		if(Speaker.isEmpty()){
+			
 			ThreadInfo listener = new ThreadInfo();
 			Listener.add(listener);
 			listener.getCondition().sleep();
 			word = listener.getWord();
+			
+		}else{
+			
+			ThreadInfo speaker = Speaker.removeFirst();
+			word = speaker.getWord();
+			speaker.getCondition().wake();
 		}
+		
 		lock.release();
-		return word;
-	}
-
-	/**
-	 * @author mark A common lock to assure the atomity of <tt>listen</tt> and
-	 *         <tt>speak</tt>
-	 */
-	private static Lock lock;
+		
+	return word;
+    }
+    
+    private static Lock lock;
 
 	private class ThreadInfo {
 		int word;
@@ -105,5 +110,6 @@ public class Communicator {
 
 	private LinkedList<ThreadInfo> Speaker;
 	private LinkedList<ThreadInfo> Listener;
+    
 }
 
